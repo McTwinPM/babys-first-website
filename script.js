8640e2e893fcd4da92c94af15090125542dd671d
Michael McCormack <mcctwin@gmail.com>
2025-03-20 20:17:54 -0500

    added name, picture, and button

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const h2 = document.createElement("h2")
 h2.textContent = "This content added by JavaScript"
 document.querySelector("body").appendChild(h2)
+
+const input = document.getElementById('button');
+input.addEventListener('click', function() {
+    alert('Just kidding!');
+});
